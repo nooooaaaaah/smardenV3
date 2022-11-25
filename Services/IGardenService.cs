73c1dfc3829de207a3ebaa4a64b8fd2957d54487
Noah <nspielman@live.com>
2022-11-25 13:15:02 -0500

    loginOrGardenServices

--- Services/IGardenService.cs
+++ Services/IGardenService.cs
@@ -2,5 +2,7 @@ using SmardenV3.Data;
 public interface IGardenService
 {
     Task<IEnumerable<GardenData>> GetGardens();
-    Task<HttpResponseMessage> PostGarden(GardenData garden); 
+    Task<HttpResponseMessage> PostGarden(GardenData garden);
+    Task<HttpResponseMessage> EditGarden(GardenData garden);
+    Task<HttpResponseMessage> DeleteGarden(int gardenID);
 }
