27823ef80bb24643994d9bf58729b94e36ec6c8d
Spielman <nspiel@colostate.edu>
2022-12-04 18:19:54 -0700

    Dashboard needs to be finised

--- Services/IUserService.cs
+++ Services/IUserService.cs
@@ -4,4 +4,9 @@ public interface IUserService
     Task<IEnumerable<UserData>> GetUsers();
     Task<HttpResponseMessage> PostUser(UserData user);
     Task<HttpResponseMessage> LoginUser(LoginData login);
+    Task<HttpResponseMessage> EditUser(UserData user);
+    Task<HttpResponseMessage> EditUserPassword(int userID, string newPassword);
+    Task<HttpResponseMessage> DeleteUser(UserData user);
+
+
 }
--- Services/UserService.cs
+++ Services/UserService.cs
@@ -16,11 +16,23 @@ namespace SmardenV3.Services
         }
         public async Task<HttpResponseMessage> PostUser(UserData user)
         {
-            return await httpClient.PostAsJsonAsync("https://localhost:7137/api/User", user);
+            return await httpClient.PostAsJsonAsync("https://localhost:7137/api/User", value: user);
+        }
+        public async Task<HttpResponseMessage> EditUser(UserData user)
+        {
+            return await httpClient.PostAsJsonAsync(requestUri: "https://localhost:7137/api/User/" + user.UserID, value: user);
+        }
+        public async Task<HttpResponseMessage> EditUserPassword(int userID, string newPassword)
+        {
+            return await httpClient.PostAsJsonAsync(requestUri: "https://localhost:7137/api/User/password/" + userID, value: newPassword);
         }
         public async Task<HttpResponseMessage> LoginUser(LoginData login)
         {
-            return await httpClient.PostAsJsonAsync("https://localhost:7137/api/login", login);
+            return await httpClient.PostAsJsonAsync("https://localhost:7137/api/login", value: login);
+        }
+        public async Task<HttpResponseMessage> DeleteUser(UserData user)
+        {
+            return await httpClient.DeleteAsync(requestUri: "https://localhost:7137/api/login/" + user.UserID);
         }
     }
 }
